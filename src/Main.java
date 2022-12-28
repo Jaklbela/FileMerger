c0609219daa6e82242068c6d0d5cbf156dba0d21
jaklbela <KsZh11@mail.ru>
2022-12-28 20:54:46 +0300

    Created the correct sorting method.

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,49 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-    static Scanner IN = new Scanner(System.in);
-    static File ROOT;
-    static Map<String, ArrayList<String>> TIE_FILES = new HashMap<>();
-    static List<String> FILES = new ArrayList<>();
+    static Map<String, ArrayList<String>> tie_files = new HashMap<>();
+    static List<String> all_files = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        findRoot();
-        readFilesInDirectory(ROOT);
-        Object[] sorted_files = makeFileList();
-        for (Object el : sorted_files) {
+        File root = findRoot();
+        readFilesInDirectory(root);
+        String[] sorted_files = makeFileList();
+        printFiles(sorted_files);
+    }
+
+    private static void printFiles (String[] sorted_files) throws IOException {
+        for (String el : sorted_files) {
+            File file = new File(el);
+            if (!file.isFile()) {
+                System.out.println("Impossible to make a list. There is no file: " + el);
+                return;
+            }
+        }
+        for (String el : sorted_files) {
             System.out.println(el);
+        }
+        System.out.println("\nLet's see the whole text!\n");
+        for (String el : sorted_files) {
+            List<String> lines = Files.readAllLines(Paths.get(el), StandardCharsets.ISO_8859_1);
+            String text = String.join(System.lineSeparator(), lines);
+            System.out.println(text);
         }
     }
 
-    private static Object[] makeFileList() {
-        List<String> files = List.copyOf(FILES);
-        Object[] sorted_files = files.toArray();
-        for (int i = 0; i < FILES.size(); ++i) {
-            ArrayList<String> list = TIE_FILES.get(FILES.get(i));
+    private static String[] makeFileList() {
+        List<String> files = List.copyOf(all_files);
+        String[] sorted_files = new String[files.size()];
+        files.toArray(sorted_files);
+        String[] keys = new String[tie_files.size()];
+        tie_files.keySet().toArray(keys);
+        for (int i = 0; i < keys.length; ++i) {
+            ArrayList<String> list = tie_files.get(keys[i]);
             if (list != null) {
                 for (String file : list) {
-                    int ind = FILES.indexOf(file);
+                    int ind = all_files.indexOf(file);
                     if (ind < i) {
-                        sorted_files[i] = FILES.get(ind);
-                        sorted_files[ind] = FILES.get(i);
+                        sorted_files[i] = all_files.get(ind);
+                        sorted_files[ind] = all_files.get(i);
                     }
                 }
             }
@@ -42,20 +60,29 @@ public class Main {
         String[] paths = folder.list();
         if (paths.length != 0) {
             for (String el : paths) {
-                String this_file = folder + "/" + el;
+                String this_file = folder + "\\" + el;
                 File path = new File(this_file);
                 if (path.isFile()) {
-                    FILES.add(this_file);
+                    all_files.add(this_file);
                     List<String> lines = Files.readAllLines(Paths.get(this_file), StandardCharsets.ISO_8859_1);
                     for (String line : lines) {
                         if (line.contains("require")) {
                             String another_file = line.substring(9, line.length() - 1) + ".txt";
-                            if (TIE_FILES.containsKey(another_file)) {
-                                TIE_FILES.get(another_file).add(this_file);
-                            } else {
+                            Set<String> keys = tie_files.keySet();
+                            boolean flag = false;
+                            if (keys.size() != 0) {
+                                for (String key : keys) {
+                                    if (key.contains(another_file)) {
+                                        tie_files.get(key).add(this_file);
+                                        flag = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (!flag) {
                                 ArrayList<String> files = new ArrayList<>();
                                 files.add(this_file);
-                                TIE_FILES.put(another_file, files);
+                                tie_files.put(another_file, files);
                             }
                         }
                     }
@@ -66,15 +93,16 @@ public class Main {
         }
     }
 
-    private static void findRoot() {
+    private static File findRoot() {
+        Scanner scanner = new Scanner(System.in);
+        File root;
         while (true) {
             System.out.println("Please input the name of root folder and make sure that it is in a program directory.");
-            String root_name = IN.nextLine();
-            ROOT = new File(root_name);
-            if (ROOT.exists())
-                break;
-            else
-                System.out.println("No such directory. Please try again.");
+            String root_name = scanner.nextLine();
+            root = new File(root_name);
+            if (root.exists())
+                return root;
+            System.out.println("No such directory. Please try again.");
         }
     }
 }
